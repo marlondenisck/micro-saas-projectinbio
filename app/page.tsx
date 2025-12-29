b486cbc5bc02d7cb77225516be7b1633b1eb06a5
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-12-28 22:02:39 -0400

    Finalizando a Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Hero from '@/app/components/landing-page/hero'
 import Header from '@/app/components/landing-page/header'
+import VideoExplanation from '@/app/components/landing-page/video-explanation'
+import Pricing from '@/app/components/landing-page/pricing'
+import FAQ from '@/app/components/landing-page/faq'
 
 export default function Home() {
   return (
     <div className='mx-auto max-w-7xl'>
       <Hero />
-
       <Header />
-      {/*<VideoExplanation />
+      <VideoExplanation />
       <Pricing />
-      <FAQ /> */}
+      <FAQ />
     </div>
   )
 }
